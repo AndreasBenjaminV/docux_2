9766f1e6cce27ed3eae47efaa967a38a95956423
Maldito Valero <abenjamin.v@gmail.com>
2017-12-19 15:50:11 -0300

    Lista de tareas (SuperHeroes + NgFor)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
+const HEROES = [
+    {id: 1, name:'Superman'},
+    {id: 2, name:'Batman'},
+    {id: 5, name:'BatGirl'},
+    {id: 3, name:'Robin'},
+    {id: 4, name:'Flash'}
+  ];
+
+  
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   constructor(public navCtrl: NavController) {
-
   }
-
+  heroes = HEROES;
 }
