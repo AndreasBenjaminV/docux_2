aa31777e8d1fce06973bbf9374c3b97feb4f6396
Maldito Valero <abenjamin.v@gmail.com>
2017-12-20 10:20:16 -0300

    implementación navparams

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { TasksPage } from '../pages/tasks/tasks';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    TasksPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    TasksPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
+//Docux 2 mobile version 
+//By Andreas Valero
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TasksPage } from '../tasks/tasks';
 
 
 const HEROES = [
@@ -8,8 +12,7 @@ const HEROES = [
     {id: 5, name:'BatGirl'},
     {id: 3, name:'Robin'},
     {id: 4, name:'Flash'}
-  ];
-
+  ];  
   
 @Component({
   selector: 'page-home',
@@ -18,5 +21,12 @@ const HEROES = [
 export class HomePage {
   constructor(public navCtrl: NavController) {
   }
-  heroes = HEROES;
+  heroes = HEROES;  
+
+  login(name_hero: any){
+    let data = {
+      name: name_hero
+    };
+    this.navCtrl.push(TasksPage,data);
+    }
 }
